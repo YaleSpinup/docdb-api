99ca69eccdfe0bef3c0ebf2db8906cf9dc8b3cc4
Tenyo Grozev <tenyo@users.noreply.github.com>
2021-11-09 14:29:11 -0500

    Support for session refresh (#11)
    * update modules
    
    * move session initialization to orchestrator; add support for refreshing the session
    
    * fixup
    
    Co-authored-by: Tenyo Grozev <tenyo.grozev@yale.edu>

--- api/handlers_docdb.go
+++ api/handlers_docdb.go
@@ -9,9 +9,6 @@ import (
 	"github.com/YaleSpinup/apierror"
 	"github.com/gorilla/mux"
 	"github.com/pkg/errors"
-
-	db "github.com/YaleSpinup/docdb-api/docdb"
-	"github.com/YaleSpinup/docdb-api/resourcegroupstaggingapi"
 )
 
 // DocumentDBCreateHandler creates a documentDB cluster and instance(s)
@@ -19,21 +16,6 @@ func (s *server) DocumentDBCreateHandler(w http.ResponseWriter, r *http.Request)
 	w = LogWriter{w}
 	vars := mux.Vars(r)
 	account := vars["account"]
-
-	role := fmt.Sprintf("arn:aws:iam::%s:role/%s", account, s.session.RoleName)
-
-	sess, err := s.assumeRole(
-		r.Context(),
-		s.session.ExternalID,
-		role,
-		"",
-		"arn:aws:iam::aws:policy/AmazonDocDBFullAccess",
-	)
-	if err != nil {
-		msg := fmt.Sprintf("failed to assume role in account: %s", account)
-		handleError(w, apierror.New(apierror.ErrForbidden, msg, err))
-		return
-	}
 
 	// read the input against our struct in api/types.go
 	req := DocDBCreateRequest{}
@@ -53,12 +35,17 @@ func (s *server) DocumentDBCreateHandler(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	orch := newDocDBOrchestrator(
-		db.New(db.WithSession(sess.Session)),
-		nil,
-		s.flywheel,
-		s.org,
+	orch, err := s.newDocDBOrchestrator(
+		r.Context(),
+		&sessionParams{
+			role:       fmt.Sprintf("arn:aws:iam::%s:role/%s", account, s.session.RoleName),
+			policyArns: []string{"arn:aws:iam::aws:policy/AmazonDocDBFullAccess"},
+		},
 	)
+	if err != nil {
+		handleError(w, errors.Wrap(err, "unable to create docdb orchestrator"))
+		return
+	}
 
 	resp, task, err := orch.documentDBCreate(r.Context(), &req)
 	if err != nil {
@@ -93,27 +80,17 @@ func (s *server) DocumentDBDeleteHandler(w http.ResponseWriter, r *http.Request)
 		}
 	}
 
-	role := fmt.Sprintf("arn:aws:iam::%s:role/%s", account, s.session.RoleName)
-
-	sess, err := s.assumeRole(
+	orch, err := s.newDocDBOrchestrator(
 		r.Context(),
-		s.session.ExternalID,
-		role,
-		"",
-		"arn:aws:iam::aws:policy/AmazonDocDBFullAccess",
+		&sessionParams{
+			role:       fmt.Sprintf("arn:aws:iam::%s:role/%s", account, s.session.RoleName),
+			policyArns: []string{"arn:aws:iam::aws:policy/AmazonDocDBFullAccess"},
+		},
 	)
 	if err != nil {
-		msg := fmt.Sprintf("failed to assume role in account: %s", account)
-		handleError(w, apierror.New(apierror.ErrForbidden, msg, err))
+		handleError(w, errors.Wrap(err, "unable to create docdb orchestrator"))
 		return
 	}
-
-	orch := newDocDBOrchestrator(
-		db.New(db.WithSession(sess.Session)),
-		nil,
-		nil,
-		s.org,
-	)
 
 	if err := orch.documentDBDelete(r.Context(), name, snapshot); err != nil {
 		handleError(w, err)
@@ -130,28 +107,20 @@ func (s *server) DocumentDBListHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	account := vars["account"]
 
-	role := fmt.Sprintf("arn:aws:iam::%s:role/%s", account, s.session.RoleName)
-
-	sess, err := s.assumeRole(
+	orch, err := s.newDocDBOrchestrator(
 		r.Context(),
-		s.session.ExternalID,
-		role,
-		"",
-		"arn:aws:iam::aws:policy/AmazonDocDBReadOnlyAccess",
-		"arn:aws:iam::aws:policy/ResourceGroupsandTagEditorReadOnlyAccess",
+		&sessionParams{
+			role: fmt.Sprintf("arn:aws:iam::%s:role/%s", account, s.session.RoleName),
+			policyArns: []string{
+				"arn:aws:iam::aws:policy/AmazonDocDBReadOnlyAccess",
+				"arn:aws:iam::aws:policy/ResourceGroupsandTagEditorReadOnlyAccess",
+			},
+		},
 	)
 	if err != nil {
-		msg := fmt.Sprintf("failed to assume role in account: %s", account)
-		handleError(w, apierror.New(apierror.ErrForbidden, msg, err))
+		handleError(w, errors.Wrap(err, "unable to create docdb orchestrator"))
 		return
 	}
-
-	orch := newDocDBOrchestrator(
-		db.New(db.WithSession(sess.Session)),
-		resourcegroupstaggingapi.New(resourcegroupstaggingapi.WithSession(sess.Session)),
-		nil,
-		s.org,
-	)
 
 	resp, err := orch.documentDBList(r.Context())
 	if err != nil {
@@ -178,27 +147,17 @@ func (s *server) DocumentDBGetHandler(w http.ResponseWriter, r *http.Request) {
 	account := vars["account"]
 	name := vars["name"]
 
-	role := fmt.Sprintf("arn:aws:iam::%s:role/%s", account, s.session.RoleName)
-
-	sess, err := s.assumeRole(
+	orch, err := s.newDocDBOrchestrator(
 		r.Context(),
-		s.session.ExternalID,
-		role,
-		"",
-		"arn:aws:iam::aws:policy/AmazonDocDBReadOnlyAccess",
+		&sessionParams{
+			role:       fmt.Sprintf("arn:aws:iam::%s:role/%s", account, s.session.RoleName),
+			policyArns: []string{"arn:aws:iam::aws:policy/AmazonDocDBReadOnlyAccess"},
+		},
 	)
 	if err != nil {
-		msg := fmt.Sprintf("failed to assume role in account: %s", account)
-		handleError(w, apierror.New(apierror.ErrForbidden, msg, err))
+		handleError(w, errors.Wrap(err, "unable to create docdb orchestrator"))
 		return
 	}
-
-	orch := newDocDBOrchestrator(
-		db.New(db.WithSession(sess.Session)),
-		nil,
-		nil,
-		s.org,
-	)
 
 	resp, err := orch.documentDBDetails(r.Context(), name)
 	if err != nil {
@@ -224,21 +183,6 @@ func (s *server) DocumentDBModifyHandler(w http.ResponseWriter, r *http.Request)
 	account := vars["account"]
 	name := vars["name"]
 
-	role := fmt.Sprintf("arn:aws:iam::%s:role/%s", account, s.session.RoleName)
-
-	sess, err := s.assumeRole(
-		r.Context(),
-		s.session.ExternalID,
-		role,
-		"",
-		"arn:aws:iam::aws:policy/AmazonDocDBFullAccess",
-	)
-	if err != nil {
-		msg := fmt.Sprintf("failed to assume role in account: %s", account)
-		handleError(w, apierror.New(apierror.ErrForbidden, msg, err))
-		return
-	}
-
 	req := DocDBModifyRequest{}
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		msg := fmt.Sprintf("cannot decode body into modify documentdb input: %s", err)
@@ -246,12 +190,17 @@ func (s *server) DocumentDBModifyHandler(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	orch := newDocDBOrchestrator(
-		db.New(db.WithSession(sess.Session)),
-		nil,
-		nil,
-		s.org,
+	orch, err := s.newDocDBOrchestrator(
+		r.Context(),
+		&sessionParams{
+			role:       fmt.Sprintf("arn:aws:iam::%s:role/%s", account, s.session.RoleName),
+			policyArns: []string{"arn:aws:iam::aws:policy/AmazonDocDBFullAccess"},
+		},
 	)
+	if err != nil {
+		handleError(w, errors.Wrap(err, "unable to create docdb orchestrator"))
+		return
+	}
 
 	resp, err := orch.documentDBModify(r.Context(), name, &req)
 	if err != nil {
